64795045eaa41389428581bff7950d977d00ffa2
Glacei <vazqueadrian689@gmail.com>
2025-02-27 17:47:22 +0100

    test 2

--- src/Treasure.java
+++ src/Treasure.java
@@ -1,3 +1,25 @@
 public class Treasure {
     
+    // Attributes
+
+    private String name;
+    private int value;
+    private double weight;
+
+    // Builders
+
+    public Treasure (String name, int value, double weight){
+        this.name = name;
+        this.value = value;
+        this.weight = weight;
+
+    } 
+
+    // Methods
+
+
+
+
+
+
 }
